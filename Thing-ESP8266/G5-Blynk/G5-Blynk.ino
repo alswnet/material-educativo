dc633c6b7212719b1d2830ed967d5bd83d161e5e
chepecarlos <chepecarlos@alswblog.org>
2017-07-05 00:06:57 -0600

    Limpiando un poco la guia

--- Thing-ESP8266/G5-Blynk/G5-Blynk.ino
+++ Thing-ESP8266/G5-Blynk/G5-Blynk.ino
@@ -1,51 +1,15 @@
-/**************************************************************
- * Blynk is a platform with iOS and Android apps to control
- * Arduino, Raspberry Pi and the likes over the Internet.
- * You can easily build graphic interfaces for all your
- * projects by simply dragging and dropping widgets.
- *
- *   Downloads, docs, tutorials: http://www.blynk.cc
- *   Blynk community:            http://community.blynk.cc
- *   Social networks:            http://www.fb.com/blynkapp
- *                               http://twitter.com/blynk_app
- *
- * Blynk library is licensed under MIT license
- * This example code is in public domain.
- *
- **************************************************************
- * This example runs on Sparkfun Blynk Board.
- *
- * You need to install this for ESP8266 development:
- *   https://github.com/esp8266/Arduino
- *
- * NOTE: You can select NodeMCU 1.0 (compatible board)
- * in the Tools -> Board menu
- *
- * Change WiFi ssid, pass, and Blynk auth token to run :)
- *
- **************************************************************/
-
-#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial //Para ver el depurador  - se puede quitar
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+char auth[] = "baf15e71d8334ec2b00062dec2d329fe";//Contraseña Blynk
+char ssid[] = "TURBONETT_ALSW";//Nombre de red
+char pass[] = "2526-4897";//Contraseña
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "baf15e71d8334ec2b00062dec2d329fe";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "ALSW";
-char pass[] = "21241324";
-
-void setup()
-{
+void setup(){
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
 }
 
-void loop()
-{
+void loop(){
   Blynk.run();
 }
-
