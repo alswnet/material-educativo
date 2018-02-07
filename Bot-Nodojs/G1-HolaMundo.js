15975f238639466423a352a12ee9c6b8325dd879
chepecarlos <chepecarlos@alswblog.org>
2018-02-07 12:15:42 -0600

    Empezando con el codigo para Bot

---
+++ Bot-Nodojs/G1-HolaMundo.js
@@ -0,0 +1,18 @@
+/*
+* sudo apt install nodejs
+*/
+
+console.log("Hola mundo");
+
+let n = 10;
+
+let p = n + 5;
+
+/*
+* mpm init
+* https://www.npmjs.com/
+* npm install twit --save
+* node bot.js
+* npm start
+* https://apps.twitter.com
+*/
