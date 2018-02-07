e80ce947b3c165d8a4ef8cc5253ecdb137463fc6
chepecarlos <chepecarlos@alswblog.org>
2018-02-07 12:46:02 -0600

    Contestar cuando se sige

--- Bot-Nodojs/G5-Segir/bot.js
+++ Bot-Nodojs/G5-Segir/bot.js
@@ -2,24 +2,42 @@ console.log('Iniciar el bot');
 
 var Twit = require('twit');
 
-tweetIt();
-setInterval(tweetIt, 1000*20);
+var config = require('./config');
+var T = new Twit(config);
 
-function tweetIt() {
+var stream = T.stream('user');
 
-	var r = Math.floor(Math.random()*100);
+var my_screen_name = null;
+
+T.get('account/verify_credentials', { skip_status: true }, function(err, data, response) {
+  my_screen_name = data.screen_name;
+  console.log('Using account ' + my_screen_name);
+});
+
+stream.on('follow', followed);
+
+function followed(eventMsg) {
+  console.log("Follow event!");
+  var name = eventMsg.source.name;
+  var screenName = eventMsg.source.screen_name;
+  if (screenName !== my_screen_name) {
+    tweetIt('.@' + screenName + ' te gusta los arduino?');
+  }
+}
+
+function tweetIt(txt) {
 
 	var tweet = {
-	  status: 'Numero Random ' + r + ' #ALSWbot'
+	  status: txt
 	}
 
 	T.post('statuses/update', tweet, tweeted);
 
 	function tweeted(err, data, response) {
 	  if (err) {
-	  	console.log("Algo Salio Mal");
+	  	console.log("Also salio mal");
 	  } else {
-	    console.log("Funciono");
+	    console.log("Todo funciono");
 	  }
 	}
 }
